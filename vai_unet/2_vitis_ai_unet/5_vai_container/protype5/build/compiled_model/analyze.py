370c4bda68b310e9f4873902caa2a48fb3ca9363
Changhong <15284244183@163.com>
2025-06-14 19:15:31 +0100

    2_5train 1_4 compile test

--- vai_unet/2_vitis_ai_unet/5_vai_container/protype5/build/compiled_model/analyze.py
+++
@@ -1,9 +0,0 @@
-import xir
-g = xir.Graph.deserialize("UnetGenerator_u50.xmodel")
-subgraphs = get_child_subgraph_dpu(g)
-
-# 打印子图输入输出关系
-for sg in subgraphs:
-    inputs = [t.name for t in sg.get_input_tensors()]
-    outputs = [t.name for t in sg.get_output_tensors()]
-    print(f"{sg.get_name()} | 输入: {inputs} -> 输出: {outputs}")
